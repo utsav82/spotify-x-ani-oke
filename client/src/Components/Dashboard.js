dfe484d35663664b05fcd9daafb3eb54261c2ca0
Malay <malaykumar2003@gmail.com>
2022-04-28 11:09:31 +0530

    commit

---
+++ client/src/Components/Dashboard.js
@@ -0,0 +1,103 @@
+import { useState, useEffect } from "react";
+import useAuth from "./utility/useAuth";
+import Player from "./Player";
+import TrackSearchResult from "./TrackSearchResult";
+import { Form } from "react-bootstrap";
+import "./css/Dashboard.css";
+import SpotifyWebApi from "spotify-web-api-node";
+import axios from "axios";
+
+import { useDataLayerValue } from "./DataLayer";
+import Header from "./Header";
+
+const spotify = new SpotifyWebApi({
+  clientId: "c0f037a261fd4f649bc7ca1338b5a3e0",
+});
+
+export default function Dashboard({ code }) {
+  const [{ token, searchResults }, dispatch] = useDataLayerValue();
+  const accessToken = useAuth(code);
+  useEffect(() => {
+    if (accessToken) {
+      dispatch({
+        type: "SET_TOKEN",
+        token: accessToken,
+      });
+      spotify.setAccessToken(accessToken);
+      spotify.getMe().then((user) => {
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
+      });
+    }
+  }, [accessToken, dispatch]);
+
+  const [playingTrack, setPlayingTrack] = useState();
+  const [lyrics, setLyrics] = useState("");
+
+  function chooseTrack(track) {
+    setPlayingTrack(track);
+    dispatch({
+      type: "SET_SEARCH_RESULTS",
+      searchResults: [],
+    });
+    setLyrics("");
+  }
+
+  // useEffect(() => {
+  //   if (!playingTrack) return;
+
+  //   axios
+  //     .get("http://localhost:3001/lyrics", {
+  //       params: {
+  //         track: playingTrack.title,
+  //         artist: playingTrack.artist,
+  //       },
+  //     })
+  //     .then((res) => {
+  //       setLyrics(res.data.lyrics);
+  //     });
+  // }, [playingTrack]);
+
+  useEffect(() => {
+    if (!token) return;
+    spotify.setAccessToken(token);
+  }, [token]);
+  useEffect(() => {
+    if (!playingTrack) return;
+    fetch(`http://127.0.0.1:5000/getdata/${playingTrack.title}`)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (text) {
+        console.log("GET response text:");
+        console.log(text);
+        setLyrics(text);
+      });
+  }, [playingTrack]);
+
+  return (
+    <div className="main-dashboard">
+      <Header spotify={spotify}></Header>
+      <div className="body">
+        {searchResults.map((track) => (
+          <TrackSearchResult
+            track={track}
+            key={track.uri}
+            chooseTrack={chooseTrack}
+          />
+        ))}
+        {console.log(searchResults.length)}
+        {searchResults.length === 0 && (
+          <div className="text-center" style={{ whiteSpace: "pre" }}>
+            {lyrics.ly}
+          </div>
+        )}
+      </div>
+      <div>
+        <Player accessToken={token} trackUri={playingTrack?.uri} />
+      </div>
+    </div>
+  );
+}
---
+++ client/src/Components/TrackSearchResult.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./css/TrackSearchResult.css";
+export default function TrackSearchResult({ track, chooseTrack }) {
+  function handlePlay() {
+    chooseTrack(track);
+  }
+
+  return (
+    <div
+      className="searchResultOption"
+      style={{ cursor: "pointer" }}
+      onClick={handlePlay}>
+      <img src={track.albumUrl} style={{ height: "64px", width: "64px" }} />
+      <div className="ml-3">
+        <div>{track.title}</div>
+        <div className="text-muted">{track.artist}</div>
+      </div>
+    </div>
+  );
+}
